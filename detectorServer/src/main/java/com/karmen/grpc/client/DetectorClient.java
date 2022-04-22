1fa8b9dbdc62a47492bbc264209d7899299f1000
longanisha <karmenlover214@gmail.com>
2022-04-22 09:18:56 +0100

    add detector client

---
+++ detectorServer/src/main/java/com/karmen/grpc/client/DetectorClient.java
@@ -0,0 +1,27 @@
+package com.karmen.grpc.client;
+
+import com.karmen.grpc.grpc.Detector;
+import com.karmen.grpc.grpc.detectorGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class DetectorClient {
+
+    public static void main(String[] args){
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9091).usePlaintext().build();
+
+        //stub from proto file
+
+        detectorGrpc.detectorBlockingStub stub =  detectorGrpc.newBlockingStub(channel);
+
+
+        Detector.DetectorRequest request = Detector.DetectorRequest.newBuilder().setIsDetected(true).build();
+
+        Detector.DetectorResponse response = stub.checkStatus(request);
+
+        System.out.println("Detector Response : "+ response.getResponsemsg());
+
+    }
+
+}
